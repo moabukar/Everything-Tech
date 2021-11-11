666729db4eb98a132c648d7a6b27e4fdbb28d9e6
mohamedA007 <mo-abukar10@hotmail.com>
2021-11-11 17:54:02 +0000

    completed python projects 1

--- Projects/PythonProject1.py
+++ Projects/PythonProject1.py
@@ -32,3 +32,13 @@ def main():
 
     # while(True):
     idx = random.randrange(0, n_movies)
+        
+    print(f'{titles[idx]} {years[idx]}, Rating: {ratings[idx]:.1f}, Starring: {actors_list[idx]}')
+
+        #user_input = input('Do you want another movie (y/[n])? ')
+        #if user_input != 'y':
+         #   break
+    
+
+if __name__ == '__main__':
+    main()
