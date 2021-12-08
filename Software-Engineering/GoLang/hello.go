cd0896ec5a79a46fc3ddb18435d2ac3da035d6a3
Mohamed Abukar <76791648+moabukar@users.noreply.github.com>
2021-12-08 10:25:36 +0000

    Create hello.go

---
+++ Software-Engineering/GoLang/hello.go
@@ -0,0 +1,9 @@
+// A hello-world for GoLang > To run, make sure you have installed GoLang and then run "go run main.go"
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world.")
+}
