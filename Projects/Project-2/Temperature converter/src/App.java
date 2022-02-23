c17195f0eaa42173e405fc46238f3d72c922ae6b
Razeen-Abdal-Rahman <98765619+Razeen-Abdal-Rahman@users.noreply.github.com>
2022-02-23 20:52:23 +0000

    Added main functionality

--- Projects/Project-2/Temperature converter/src/App.java
+++ Projects/Project-2/Temperature converter/src/App.java
@@ -5,24 +5,35 @@ public class App {
         Scanner input = new Scanner(System.in);
         System.out.println("This applicaion converts from degrees Celsius to degrees Fahrenheit and vice versa");
         System.out.println("Would you like to input Celsius or Fahrenheit?");
-        System.out.println("Enter 'C' for Celsius or 'F' for Fahrenheit: ");
-        String a = input.next();
-        if (a == "C"){
+        System.out.println("Enter 'C' for Celsius or 'F' for Fahrenheit: ");    //input must be a single capital letter
+        char a = input.next().charAt(0);
+        if (a == 'C'){
             System.out.println("Plese eneter the temperature in degrees Celsius (as a number without the sign) that you would like to convert to Fahrenheit");
-            //TODO add try catch if user inputs non int value
-            int C = input.nextInt();
-            Double F = (C*1.8) + 32;    //Satandard calculation to convert from Celsius to Fahrenheit
-
+            try{
+                int C = input.nextInt();
+                Double F = (C*1.8) + 32;    //Satandard calculation to convert from Celsius to Fahrenheit
+                System.out.println(F.toString()+"°F");
+            }
+            catch(Exception e){
+                System.out.println("Please make sure you enter a number");
+            }
         }
-        if(a == "F"){
+
+        else if(a == 'F'){
             System.out.println("Plese eneter the temperature in degrees Fahrenheit (as a number without the sign) that you would like to convert to Celsius");
-            //TODO add try catch if user inputs non int value
-            int F = input.nextInt();
-            Double C = (F-32) / 1.8;    //Standard calcuation to convert from Fahrenheit to Celsius
-
+            try{
+                int F = input.nextInt();
+                Double C = (F-32) / 1.8;    //Standard calcuation to convert from Fahrenheit to Celsius
+                System.out.println(C.toString()+"°C");
+            }
+            catch(Exception e){
+                System.out.println("Please make sure you enter a number");
+            }
         }
+
         else{
             System.out.println("Sorry input was incorrect application terminating");
         }
+        input.close();
     }
 }
